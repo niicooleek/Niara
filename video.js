68483d36b8e0ff8e1309ee0b73c00f29345969c5
morado15 <morado15@morado15.id.dis.ibero.mx>
2019-09-30 11:02:27 -0500

    no lo logre
    prdn

---
+++ video.js
@@ -0,0 +1,47 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('video', {
+        height: '360',
+        width: '640',
+        videoId: 'M7lc1UVf-VE',
+        playersVars: { 'autoplay': 0 },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+var isPlay = false;
+window.addEventListener('scroll', function () {
+
+    var video = document.getElementById("video");
+    var posvideo = video.offsetTop;
+    var alturavideo = video.offsetHeight;
+    var currentScroll = window.pageYOffset;
+    var windowh = window.innerHeight;
+
+    if (posvideo + alturavideo < currentScroll + windowh && posvideo > currentScroll) {
+        if (isPlay) {
+            player.playVideo();
+            isPlay = false;
+        }
+    } else {
+        if (isPlay) {
+            player.pauseVideo();
+            isPlay = false;
+        }
+    }
+}
+    /* console.log("posvideo", posvideo);
+        console.log("currentScroll", currentScroll);
+        console.log("windowh", windowh);*/
+);
